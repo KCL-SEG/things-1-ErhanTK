47639c9390710b366e3c50f49084c8f91a69fd5b
ErhanTK <125463792+ErhanTK@users.noreply.github.com>
2023-12-08 14:09:24 +0000

    Revert "Update model"
    This reverts commit 7b193234977e394e0cec0f392110ab70705db77f.

--- things/models.py
+++ things/models.py
@@ -1,7 +1,10 @@
-from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator, MaxLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Thing(models.Model):
-    name = models.TextField(max_length=30, blank=False, unique=True, validators=[MinLengthValidator(0), MaxLengthValidator(30)])
-    description = models.TextField(max_length=120, blank=True, validators=[MinLengthValidator(0), MaxLengthValidator(120)])
-    quantity = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    name = models.CharField(max_length=35, unique=True)
+    description = models.CharField(max_length=120, blank=True)
+    quantity = models.IntegerField(
+        validators=[MinValueValidator(0),MaxValueValidator(50)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)

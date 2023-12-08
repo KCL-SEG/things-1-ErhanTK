7b193234977e394e0cec0f392110ab70705db77f
ErhanTK <125463792+ErhanTK@users.noreply.github.com>
2023-12-08 13:59:24 +0000

    Update model

--- things/models.py
+++ things/models.py
@@ -1,10 +1,7 @@
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator, MaxLengthValidator
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=35, unique=True)
-    description = models.CharField(max_length=120, blank=True)
-    quantity = models.IntegerField(
-        validators=[MinValueValidator(0),MaxValueValidator(50)]
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.TextField(max_length=30, blank=False, unique=True, validators=[MinLengthValidator(0), MaxLengthValidator(30)])
+    description = models.TextField(max_length=120, blank=True, validators=[MinLengthValidator(0), MaxLengthValidator(120)])
+    quantity = models.PositiveIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
